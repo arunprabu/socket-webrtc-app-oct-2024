1ee4199dca23ef6f3616a43b1136ac9bd6920ca0
Arun Vijayarengan <arunprabu.v@hotmail.com>
2024-10-08 10:00:16 +0530

    updated

--- server.js
+++ server.js
@@ -7,7 +7,36 @@ const server = http.createServer(app);
 const io = new Server(server); // creating object of Server with express server
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get("/text-chat", (req, res) => {
+  res.sendFile(__dirname + "/public/text-chat.html");
+});
+
+app.get("/video-chat", (req, res) => {
+  res.sendFile(__dirname + "/public/video-chat.html");
+});
+
+// To establish socket io connection -- front needs socket.io js script-- Refer text-chat.html
+// Handle connection events
+// Step 1: (Refer text-chat.html for step 2)
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  // listening to all custom events emitted By front end 
+  socket.on("chat_message",  (message) => {
+    console.log(message); // receiving the one user's chat message
+
+    // i want to send this message to all clients (users)
+    io.emit("message_from_server", message);
+
+  });
+
+  // handle disconnection
+  socket.on("disconnect", () => {
+    console.log('User disconnected');
+  });
 });
 
 server.listen(3002, () => {
